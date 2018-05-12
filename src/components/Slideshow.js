bf13b88449a8d2da95bfe71f7542dbf5ed0ea53f
Michael Dunlap <michaeldunlap@earthlink.net>
2018-05-12 11:12:16 -0700

    state-enabled slideshow

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -4,11 +4,26 @@ import ReactDOM from 'react-dom'
 class Slideshow extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      slideIndex: 0
+    }
+    this.updateSlide = this.updateSlide.bind(this)
+    this.interval = setInterval(this.updateSlide, 4000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  updateSlide() {
+    this.setState((prevState, props) => ({
+      slideIndex: prevState.slideIndex !== this.props.children.length - 1 ? prevState.slideIndex + 1 : 0
+    }))
   }
   render() {
     return (
       <div style={{height:'100%',margin:'0'}}>
-        {this.props.children[0]}
+        {this.props.children[this.state.slideIndex]}
       </div>
     )
   }
